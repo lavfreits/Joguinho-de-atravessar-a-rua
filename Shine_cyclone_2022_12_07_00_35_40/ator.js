d66b8013a3c28c629d0629f4c5f08f416d8870e3
Lavínia Lima de Freitas <119093689+lavfreits@users.noreply.github.com>
2022-12-06 21:37:32 -0300

    Add files via upload

---
+++ Shine_cyclone_2022_12_07_00_35_40/ator.js
@@ -0,0 +1,49 @@
+let xAtor = 100;
+let yAtor = 366;
+let colisao = false;
+let meusPontos = 0;
+
+function mostraAtor(){
+  image(imagemDoAtor, xAtor, yAtor, 30, 30);
+}
+
+function movimentaAtor(){
+  if (keyIsDown(UP_ARROW)){
+    yAtor -= 3;
+  }
+  if (keyIsDown(DOWN_ARROW)){
+    if(podeSeMover()){
+    yAtor += 3;
+  }}
+}
+function verificaColisao(){
+  for (let i = 0; i < imagemCarros.length; i++){
+    colisao = collideRectCircle(xCarros[i], yCarros[i], comprimentoCarro, alturaCarro, xAtor, yAtor, 15 )
+    if (colisao){
+      voltaPInicio()
+      somDaColisao.play();
+      if (pontosMaiorque0()){
+        meusPontos -= 1;
+      } }}}
+function voltaPInicio(){
+  yAtor = 366;
+  }
+function incluiPontos(){
+  textAlign(CENTER);
+  textSize(25);
+  fill(color(255, 255, 255));
+  text(meusPontos,width / 5, 27);
+}
+function marcaPonto(){
+  if (yAtor < 15){
+    meusPontos += 1;
+    somDoPonto.play();
+    voltaPInicio();
+  } 
+}
+function pontosMaiorque0(){
+  return meusPontos > 0 
+}
+function podeSeMover(){
+  return yAtor < 366;
+}
